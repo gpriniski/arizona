3c83e70e9e54794c1eaab3584c0bf4111d6b68d6
George Priniski <priniskigeorge@gmail.com>
2016-05-17 23:10:35 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Arizona::Application.config.secret_key_base = '3cddae077506f1601e0ee73fa1da67b9c7c21566f33794adf62c5eb006bf7b3bd277290258b63b82de3d61fe9cde92e5c8a62bfeed8f5ea99176186ba314ad8b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Arizona::Application.config.session_store :cookie_store, key: '_arizona_session'
